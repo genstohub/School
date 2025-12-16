5bc862c2641aaeda641c2f420ce5a76bf9500dd1
Wasem1209 <philipwasem3@gmail.com>
2025-12-16 09:00:03 +0100

    Getting updates

--- frontend/src/app/(public)/Components/SignInForm.tsx
+++ frontend/src/app/(public)/Components/SignInForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-
+import { useAuth } from "@/context/AuthContext";
+import { RegisterPayload } from "@/api/auth";
 import { useRouter } from "next/navigation";
 
 const SignInForm: React.FC = () => {
